6f8105f9640c862de67962065525e282db87e751
eckonuke <xkyix38@gmail.com>
2023-02-01 18:06:38 +0900

    Initial commit

---
+++ Source/AngryBirdVR/Private/KYI_SlingShot.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KYI_SlingShot.h"
+
+// Sets default values
+AKYI_SlingShot::AKYI_SlingShot()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AKYI_SlingShot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AKYI_SlingShot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
